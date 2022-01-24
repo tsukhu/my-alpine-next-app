ab11bbf8a73bf46e7d24afb5eeb2c7f25d9763bf
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2022-01-24 17:36:03 +0530

    updated with react components

--- components/helloWorld.jsx
+++ components/helloWorld.jsx
@@ -1,12 +1,12 @@
 export default function HelloWorld({ data }) {
     return (
-        <>
+        <div className="flex flex-col w-full items-center">
             {data.map((d) => {
-                return (<div key={d.key}>
+                return (<div key={d.key} className="bg-purple-700 text-white m-2 p-2 w-full rounded"> 
                     {d.name}
                 </div>)
             })
             }
-        </>
+        </div>
     )
 }
